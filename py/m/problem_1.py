b9a98bd13f0143bb7d5209741a701462bccbb427
Marco Nicolini <marco.nicolini@gmail.com>
2017-09-19 00:16:35 +0200

    Added problem 1 (w/stubs for comparison)

---
+++ py/m/problem_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://projecteuler.net/problem=1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+#
+# take 1, naive O(n)
+#
+def solve2(n=1000, a=3, b=5):
+    sum = 0
+    i = 0
+    while i < n:
+        if (i % a == 0) or (i % b == 0):
+            sum += i
+        i += 1
+    return sum
+
+#
+# take 2, using n (n + 1) / 2 and no loops, O(1)
+# 
+def sum_of_first_n_ints(n):
+    return n * (n + 1) // 2;
+
+def sum_of_mults(n, mult):
+    terms = (n // mult) - 1 if n % mult == 0 else n // mult
+    return mult * sum_of_first_n_ints(terms)
+
+def solve(n=1000, a=3, b=5):
+    return sum_of_mults(n, a) + sum_of_mults(n, b) - sum_of_mults(n, a * b) # assumes a and b have no common factor.
+
+if __name__ == "__main__":
+    print solve()
+    print solve2()
+    
---
+++ py/race.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Loads and confront the results for different implementations of the euler problems
+
+import imp
+import os
+import time
+
+MIN_SOLVE_TIME_MILLIS = 1000
+
+def current_time_millis():
+    return int(round(time.time() * 1000))
+
+def load_and_perform_solve(author_dir, filename):
+    '''
+    Loads a python module from the author dir, assuming it contains a function name solve without args.
+    Calls solve() over and over until MIN_SOLVE_TIME_MILLIS are passed.
+    Returns a result object.
+    '''
+    basename = os.path.basename(filename)
+    solver = imp.load_source("%s_%s" % (author_dir, basename), os.path.join(author_dir, filename))
+
+    iteration = 0
+    start_time = current_time_millis()
+    while True:
+        iteration += 1
+        result = solver.solve()
+        elapsed = current_time_millis() - start_time;
+        if (elapsed > MIN_SOLVE_TIME_MILLIS):
+            break
+
+    return Result(author=author_dir, problem_name=basename, result=result, elapsed_millis=elapsed, iterations=iteration)
+
+class Result():
+    def __init__(self, author=None, problem_name=None, result=None, elapsed_millis=0, iterations=1):
+        self.author = author
+        self.problem_name = problem_name
+        self.result = result
+        self.elapsed_millis = elapsed_millis
+        self.iterations = iterations
+
+    def score(self):
+        '''
+        Score is the number of iterations for unit of time.
+        '''
+        return float(self.iterations) / float(self.elapsed_millis)
+
+    def __str__(self):
+        return "[%s@%s] result [%s] elapsed [%d] iterations [%d] score [%f]" % (self.author, self.problem_name, str(self.result), self.elapsed_millis, self.iterations, self.score())
+
+def enumerate_w_first(items):
+    for i, item in enumerate(items):
+        yield i == 0, item
+
+def compare_solutions():
+    # look for all *.py files in dirs /v and /m. Take only the ones that appear in both dirs.
+    authors_dirs = ['m', 'v']
+    problems = map(lambda dir: set(filter(lambda x: x.endswith('.py'), os.listdir(dir))), authors_dirs)
+    problems = reduce(lambda p1, p2: p1 & p2, problems)
+
+    print('found [%d] problems implementations to compare...' % len(problems))
+
+    for problem in problems:
+        print("-----------------------------------------------------")
+        print(problem)
+        print("-----------------------------------------------------")
+        results = []
+        for author in authors_dirs:
+            print("... running %s by %s" % (problem, author))
+            results.append(load_and_perform_solve(author, problem))
+
+        # faster (i.e. higher iterations/elapsed time) wins.
+        sorted_results = sorted(results, key=lambda x: x.score(), reverse=True)
+        for winner, result in enumerate_w_first(sorted_results):
+            if winner:
+                print("%s wins!" % result.author)
+            print(str(result))    
+
+if __name__ == "__main__":
+    compare_solutions()
