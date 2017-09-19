11fd912b5d42bb5181fe09c8ea5075491c313b8b
Valeria Luppi <32057678+vluppi@users.noreply.github.com>
2017-09-19 11:07:58 +0200

    Updated problem_1.py

--- py/v/problem_1.py
+++ py/v/problem_1.py
@@ -5,8 +5,17 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def solve(n=1000, a=3, b=5):
-    return 42
+def solve()
+	blocchetto = 195 #somma dei multipli di 3 e/o di 5 minori di 30
+	somma = blocchetto
+
+	for item in range(1,33):
+		blocchetto = blocchetto+30*14
+		somma = somma + blocchetto
+	
+	somma = somma +990+993+995+996+999
+	
+	return somma
 
 if __name__ == "__main__":
     print solve()
