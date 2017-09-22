9fbf8352e9a76067625caea68ae8c0bdd0aac639
Valeria Luppi <32057678+vluppi@users.noreply.github.com>
2017-09-22 16:20:04 +0200

    Added problem 4

---
+++ py/v/problem_4.py
@@ -0,0 +1,20 @@
+def solve():
+	for digit3 in range(999,100,-1):
+		digit6 = (int)('%d%s' % (digit3,str(digit3)[::-1]))
+
+		for divisore in range(999,100,-1):
+			if (digit6 % divisore == 0 and digit6/divisore < 999):
+				print digit6, "divisore: ",divisore
+				return digit6
+	return 0
+  
+race = {
+	'author': 'valeria',
+	'problemName': '4',
+	'raceables': { 
+		'blocchetto': solve
+	}  
+}
+
+if __name__ == "__main__":
+    print solve()
