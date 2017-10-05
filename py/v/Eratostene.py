13c7bca77826c2301ab79c4df316b00f14f69fcb
Valeria Luppi <v.luppi@be-tse.it>
2017-10-05 19:08:32 +0200

    Polish

--- py/v/Eratostene.py
+++ py/v/Eratostene.py
@@ -1,12 +1,7 @@
 import math
 
 def Eratostene(N):
-
 	setaccio = range(2,N)
-	print (int)(math.sqrt(N))
-	
 	for i in range(2,(int)(math.sqrt(N))+1):
-		print "i: ", i
 		setaccio = list(filter((lambda x: (x%i!=0 or x==i)), setaccio))			
-		
-Eratostene(100)
+	return setaccio
