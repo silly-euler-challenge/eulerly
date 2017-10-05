44efcb4fc467ec75ab8800b1758a8397253ad88c
Marco Nicolini <marco.nicolini@gmail.com>
2017-10-05 23:26:05 +0200

    Optimize! Optimize!

--- py/v/Eratostene.py
+++ py/v/Eratostene.py
@@ -1,7 +1,82 @@
 import math
 
-def Eratostene(N):
+def Eratostene(N=1000000):
 	setaccio = range(2,N)
 	for i in range(2,(int)(math.sqrt(N))+1):
 		setaccio = list(filter((lambda x: (x%i!=0 or x==i)), setaccio))			
 	return setaccio
+
+def Eratostene2(N=1000000):
+	setaccio = filter((lambda x: (x%2!=0 or x==2)), range(2, N))
+	sqrt_n = int(math.sqrt(N)) + 1
+	for i in xrange(3, sqrt_n, 2):
+		setaccio = filter((lambda x: (x%i!=0 or x==i)), setaccio)
+	return setaccio
+
+def NonEraTostene(n=1000000):
+	primi = [2]
+	len_primi = 1
+	for i in xrange(3, n, 2):
+		sqrt_of_i = int(math.sqrt(i)) + 1
+		k = 0
+		is_prime = True
+		while k < len_primi and (primi[k] <= sqrt_of_i):
+			if i % primi[k] == 0:
+				is_prime = False
+			k += 1
+
+		if is_prime:
+			primi.append(i)
+			len_primi += 1
+
+	return primi
+
+#
+# Generator version
+#
+
+def primes_xrange(nth_prime):
+	'''Like xrange(n) but gives you the first n primes'''
+	primi = [2]
+	len_primi = 1
+
+	if nth_prime <= 0:
+		return
+
+	yield 2
+	if nth_prime == 1:	
+		return
+
+	i = 3
+	while len_primi < nth_prime:
+		sqrt_of_i = int(math.sqrt(i)) + 1
+		k = 1
+		is_prime = True
+		while k < len_primi and (primi[k] <= sqrt_of_i):
+			if i % primi[k] == 0:
+				is_prime = False
+			k += 1
+
+		if is_prime:
+			primi.append(i)
+			len_primi += 1
+			yield i
+
+		i+=2
+
+
+race = {
+    'problemName': 'eratostene',
+    'author': 'valeria & marco',
+    'raceables': { 
+        'eratostene_uno': Eratostene,
+        'eratostene_due': Eratostene2,
+        'non_era_tostene': NonEraTostene
+    }  
+}
+
+if __name__ == '__main__':
+	un_milione = 1000000
+	Eratostene(un_milione)
+	Eratostene2(un_milione)
+	NonEraTostene(un_milione)
