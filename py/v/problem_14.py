58a018a507e31118cc7a9e78d264dadcf0e59cf8
Valeria Luppi <v.luppi@be-tse.it>
2017-10-24 20:34:27 +0200

    Solved problem 14

--- py/v/problem_14.py
+++ py/v/problem_14.py
@@ -1,5 +1,3 @@
-import time
-
 def collatz(n, cammino) :
   if (n == 1):
     return cammino
@@ -11,18 +9,25 @@ def collatz(n, cammino) :
     return collatz((3 * n + 1) / 2, cammino)
 	
 	
-def collatz_con_memoria(N, n, cammino, memoria):
-  if (n == 1):
-    memoria[N] = cammino
-    return cammino
-  if( n < N ):
-    return (memoria[n] + cammino)
+def collatz_con_memoria(N, n, cammino_parziale, memoria):
+  '''
+  N : primissimo punto di partenza della successione
+  n : ultimo punto calcolato della successione
+  cammino_parziale : lunghezza del cammino fatto fin qui
+  memoria : per ogni intero di cui ho calcolato la sequenza, memorizza la lunghezza del cammino corrispondente
+  '''
+  if(n == 1):
+    memoria[N] = cammino_parziale
+    return cammino_parziale, memoria
+  if(n in memoria):
+    memoria[N] = memoria[n]+cammino_parziale
+    return cammino_parziale, memoria
   if(n % 2 == 0):
-    cammino += 1
-    return collatz_con_memoria(N, n / 2, cammino, memoria)
+    cammino_parziale += 1
+    return collatz_con_memoria(N, n / 2, cammino_parziale, memoria)
   else:
-    cammino += 2
-    return collatz_con_memoria(N, (3 * n + 1) / 2, cammino, memoria)
+    cammino_parziale += 2
+    return collatz_con_memoria(N, (3 * n + 1) / 2, cammino_parziale, memoria)
 	
 	
 def very_brute():
@@ -42,11 +47,11 @@ def better():
   cammino_max = 0
   n_max = 1
   memoria = {}
-  
+   
   for n in xrange(1,1000000):
-    cammino = collatz_con_memoria(n, n, 0, memoria)
-    if(cammino > cammino_max):
-      cammino_max = cammino
+    cammino, memoria = collatz_con_memoria(n, n, 0, memoria)
+    if(memoria[n] > cammino_max):
+      cammino_max = memoria[n]
       n_max = n
   return n_max, cammino_max  
  
@@ -56,11 +61,23 @@ n_max, cammino_max = very_brute()
 end = time.time()
 print "max: ", n_max, " - ", cammino_max
 print "tempo: ", (end-start)
-'''
 
 start = time.time()
 n_max, cammino_max = better()
 end = time.time()
 print "max: ", n_max, " - ", cammino_max
 print "tempo: ", (end-start)
+'''
+
+race = {
+    'problemName': '14',
+    'author': 'valeria',
+    'raceables': { 
+	    'very_brute': very_brute
+        'better': better
+    }
+}
+
+if __name__ == "__main__":
+  print better()
 
