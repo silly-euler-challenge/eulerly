f8e9889b2d8c4868a4eee2f84c0eb7f60d69ed79
Erik Cittadino <erik.cittadino@bancaimi.com>
2018-11-30 16:35:14 +0100

    Added solution to problem 3

---
+++ py/e/problem_3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://projecteuler.net/problem=3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+def is_prime(n):
+    if n <= 3:
+        return n > 1
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i=5
+    while i*i <= n:
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+        i = i + 6
+    return True
+
+def euler(n):
+    factors = []
+    i = 1
+    while n != 1 :
+        i += 1
+        if n % i == 0 and is_prime(i):
+            factors.append(i)
+            n //= i
+    return factors
+
+def solve(n=600851475143):
+    euler(n)
+
+race = {
+    'problemName': '3',
+    'author': 'erik',
+    'raceables': {
+        'solution':  solve
+    }
+}
+
+if __name__ == "__main__":
+    print solve()
